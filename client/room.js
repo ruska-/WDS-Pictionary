ec222886da307c0a61a8cae71f4013112f229c00
ruska- <ruska.beridze@gmail.com>
2024-01-31 13:45:34 +0400

    added functionality to go back to index page when url is broken and no room or name is present

--- client/room.js
+++ client/room.js
@@ -4,3 +4,11 @@ const production = process.env.NODE_ENV === "production";
 const serverURL = production ? "realsite.com" : "http://localhost:3000";
 
 const socket = io(serverURL);
+
+const urlParams = new URLSearchParams(window.location.search);
+const name = urlParams.get("name");
+const roomId = urlParams.get("room-id");
+
+if (!name || !roomId) window.location = "/index.html";
+
+
